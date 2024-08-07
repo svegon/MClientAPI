d01e95465ebf30fefeb7b6eac154830128fe46d0
Svegon <58487451+svegon@users.noreply.github.com>
2024-08-07 11:26:12 +0200

    Discontinuing this branch for newer version

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export JAVA_HOME="$JAVA17_HOME"
-
-cd ..
-./gradlew build
-mv build/libs/
--- scripts/gen_sources.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export JAVA_HOME="$JAVA17_HOME"
-cd ..
-./gradlew genSources --stacktrace
-./gradlew build
