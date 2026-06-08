86868fb08d02a050d9e0c6dfd7bd7fdda57e7259
Svegon <svegon12@gmail.com>
2026-06-08 02:06:05 +0200

    Source update
    I removed mod sources for dedicated server runtime.

--- src/server/kotlin/io/github/svegon/mclientapi/server/MClientAPIServerMain.kt
+++
@@ -1,9 +0,0 @@
-package io.github.svegon.mclientapi.server
-
-import net.fabricmc.api.DedicatedServerModInitializer
-
-class MClientAPIServerMain : DedicatedServerModInitializer {
-    companion object {}
-
-    override fun onInitializeServer() {}
-}
